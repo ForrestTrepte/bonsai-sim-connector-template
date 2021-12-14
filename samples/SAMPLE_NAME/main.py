f520816e1a11dd3e148ebc4fd94149277055cf2a
Forrest Trepte <forrest@microsoft.com>
2021-12-14 12:18:29 -0800

    stripped down main

--- samples/SAMPLE_NAME/main.py
+++ samples/SAMPLE_NAME/main.py
@@ -1,72 +1,49 @@
-#!/usr/bin/env python3
-
 import os
 import time
-import sys
 from microsoft_bonsai_api.simulator.client import BonsaiClient, BonsaiClientConfig
 from microsoft_bonsai_api.simulator.generated.models import SimulatorInterface, SimulatorState, SimulatorSessionResponse
 
-# Add parent directory containing the connector folder to path.
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+workspace = os.getenv("SIM_WORKSPACE")
+accesskey = os.getenv("SIM_ACCESS_KEY")
 
-from connector.simulator_model import SimulatorModel
+config_client = BonsaiClientConfig()
+client = BonsaiClient(config_client)
 
-def main():
-    """
-    Creates a Bonsai simulator session and executes Bonsai episodes.
-    """
+registration_info = SimulatorInterface(
+    name="SAMPLE_NAME-sim",
+    timeout=60,
+    simulator_context=config_client.simulator_context,
+    description=None,
+)
 
-    workspace = os.getenv("SIM_WORKSPACE")
-    accesskey = os.getenv("SIM_ACCESS_KEY")
+registered_session: SimulatorSessionResponse = client.session.create(workspace_name=config_client.workspace, body=registration_info)
+print(f"Registered simulator. {registered_session.session_id}")
 
-    config_client = BonsaiClientConfig()
-    client = BonsaiClient(config_client)
+sequence_id = 1
+sim_model = SimulatorModel()
+sim_model_state = { 'sim_halted': False }
 
+try:
+    while True:
+        sim_state = SimulatorState(sequence_id=sequence_id, state=sim_model_state, halted=sim_model_state.get('sim_halted', False))
+        event = client.session.advance(
+            workspace_name=config_client.workspace,
+            session_id=registered_session.session_id,
+            body=sim_state,
+        )
+        sequence_id = event.sequence_id
 
-    registration_info = SimulatorInterface(
-        name="SAMPLE_NAME-sim",
-        timeout=60,
-        simulator_context=config_client.simulator_context,
-        description=None,
-    )
-
-    print(f"config: {config_client.server}, {config_client.workspace}")
-    registered_session: SimulatorSessionResponse = client.session.create(workspace_name=config_client.workspace, body=registration_info)
-    print(f"Registered simulator. {registered_session.session_id}")
-
-    sequence_id = 1
-    sim_model = SimulatorModel()
-    sim_model_state = { 'sim_halted': False }
-
-    try:
-        while True:
-            sim_state = SimulatorState(sequence_id=sequence_id, state=sim_model_state, halted=sim_model_state.get('sim_halted', False))
-            event = client.session.advance(
-                workspace_name=config_client.workspace,
-                session_id=registered_session.session_id,
-                body=sim_state,
-            )
-            sequence_id = event.sequence_id
-            print(f'[{time.strftime("%H:%M:%S")}] Last Event: {event.type}')
-
-            if event.type == "Idle":
-                time.sleep(event.idle.callback_time)
-            elif event.type == "EpisodeStart":
-                print(f"config {event.episode_start.config}")
-                sim_model_state = sim_model.reset(event.episode_start.config)
-                print(f"state {sim_model_state}")
-            elif event.type == "EpisodeStep":
-                print(f"action {event.episode_step.action}")
-                sim_model_state = sim_model.step(event.episode_step.action)
-                print(f"state {sim_model_state}")
-            elif event.type == "EpisodeFinish":
-                sim_model_state = { 'sim_halted': False }
-            elif event.type == "Unregister":
-                print(f"Simulator Session unregistered by platform because '{event.unregister.details}'")
-                return
-    except BaseException as err:
-        client.session.delete(workspace_name=config_client.workspace, session_id=registered_session.session_id)
-        print(f"Unregistered simulator because {type(err).__name__}: {err}")
-
-if __name__ == "__main__":
-    main()
+        if event.type == "Idle":
+            time.sleep(event.idle.callback_time)
+        elif event.type == "EpisodeStart":
+            sim_model_state = sim_model.reset(event.episode_start.config)
+        elif event.type == "EpisodeStep":
+            sim_model_state = sim_model.step(event.episode_step.action)
+        elif event.type == "EpisodeFinish":
+            sim_model_state = { 'sim_halted': False }
+        elif event.type == "Unregister":
+            print(f"Simulator Session unregistered by platform because '{event.unregister.details}'")
+            return
+except BaseException as err:
+    client.session.delete(workspace_name=config_client.workspace, session_id=registered_session.session_id)
+    print(f"Unregistered simulator because {type(err).__name__}: {err}")
